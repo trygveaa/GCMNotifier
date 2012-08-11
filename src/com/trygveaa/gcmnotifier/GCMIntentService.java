e9f46840560692eb5a84e0454fbd1cb097c63dd4
Trygve Aaberge <trygveaa@gmail.com>
2012-08-11 22:28:36 +0200

    Added some log messages to the service

--- src/com/trygveaa/gcmnotifier/GCMIntentService.java
+++ src/com/trygveaa/gcmnotifier/GCMIntentService.java
@@ -2,32 +2,33 @@ package com.trygveaa.gcmnotifier;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gcm.GCMBaseIntentService;
 
 public class GCMIntentService extends GCMBaseIntentService {
 
-	@Override
-	protected void onError(Context arg0, String arg1) {
-		// TODO Auto-generated method stub
+	private static final String TAG = "GCMNotifier";
 
+	@Override
+	protected void onError(Context context, String errorId) {
+		Log.d(TAG, "Error occured: " + errorId);
 	}
 
 	@Override
-	protected void onMessage(Context arg0, Intent arg1) {
-		// TODO Auto-generated method stub
-
+	protected void onMessage(Context context, Intent intent) {
+		Bundle data = intent.getExtras();
+		Log.d(TAG, "Message received: " + data.getString("title") + " " + data.getString("body"));
 	}
 
 	@Override
-	protected void onRegistered(Context arg0, String arg1) {
-		// TODO Auto-generated method stub
-
+	protected void onRegistered(Context context, String registrationId) {
+		Log.v(TAG, "Device registered: " + registrationId);
 	}
 
 	@Override
-	protected void onUnregistered(Context arg0, String arg1) {
-		// TODO Auto-generated method stub
-
+	protected void onUnregistered(Context context, String registrationId) {
+		Log.v(TAG, "Device unregistered: " + registrationId);
 	}
 }
