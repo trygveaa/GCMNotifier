3e6300df11132b92fe5dd9eacda9b26d14b93685
Trygve Aaberge <trygveaa@gmail.com>
2012-08-11 21:07:22 +0200

    Add receiver and service for GCM

---
+++ src/com/trygveaa/gcmnotifier/GCMIntentService.java
@@ -0,0 +1,33 @@
+package com.trygveaa.gcmnotifier;
+
+import android.content.Context;
+import android.content.Intent;
+
+import com.google.android.gcm.GCMBaseIntentService;
+
+public class GCMIntentService extends GCMBaseIntentService {
+
+	@Override
+	protected void onError(Context arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void onMessage(Context arg0, Intent arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void onRegistered(Context arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void onUnregistered(Context arg0, String arg1) {
+		// TODO Auto-generated method stub
+
+	}
+}
