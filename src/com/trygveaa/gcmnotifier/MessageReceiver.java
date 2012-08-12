7a0645f85890517790cf2ae1353002849d33b1fa
Trygve Aaberge <trygveaa@gmail.com>
2012-08-12 18:46:07 +0200

    Send custom intent when clicking notification

--- src/com/trygveaa/gcmnotifier/MessageReceiver.java
+++ src/com/trygveaa/gcmnotifier/MessageReceiver.java
@@ -4,6 +4,9 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 
 public class MessageReceiver extends BroadcastReceiver {
 
@@ -11,10 +14,30 @@ public class MessageReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		String action = intent.getAction();
 		if (action != null) {
+			SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+
 			if (action.equals("com.trygveaa.gcmnotifier.CANCEL_NOTIFICATIONS")) {
 				cancelNotifications(context);
+
 			} else if (action.equals("com.trygveaa.gcmnotifier.CLICK_NOTIFICATION")) {
 				cancelNotifications(context);
+				String clickAction = sharedPreferences.getString(context.getString(R.string.intent_click_action_key),
+						context.getString(R.string.intent_click_action_default));
+
+				if (!clickAction.isEmpty()) {
+					Intent clickIntent;
+					String clickUri = sharedPreferences.getString(context.getString(R.string.intent_click_uri_key),
+							context.getString(R.string.intent_click_uri_default));
+
+					if (!clickUri.isEmpty()) {
+						clickIntent = new Intent(clickAction, Uri.parse(clickUri));
+					} else {
+						clickIntent = new Intent(clickAction);
+					}
+
+					clickIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					context.startActivity(clickIntent);
+				}
 			}
 		}
 	}
